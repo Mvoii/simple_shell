7d1b0e4981fd4bf3b44d26c03f4b6dbe6c189c71
Franklin Mvoi <mvoifranklin@gmail.com>
2024-02-23 17:53:56 +0300

    initial commit

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H /*MAIN_H*/
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <unistd.h>
+#include <fcnt.h>
+#include <signal.h>
+
+#include <sys/wait.h>
+#include <sys/types.h>
+
+#endif /*MAIN_H*/
